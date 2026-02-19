9903cec74a5e4501794e63192a43c48825fb18a6
rprohitpapnai <rprohitpapnai@gmail.com>
2026-02-20 00:48:16 +0530

    finally the database connection is working

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import {DB_NAME} from "../constants.js"
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const conncectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n mongo db conncected db host : ${conncectionInstance.connection.host}`)
     }
     catch(error){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-//require('dotenv').config({path:"./env"})
-
+// require('dotenv').config({path:"./.env"})
 import dotenv from "dotenv"
+
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js"; 
